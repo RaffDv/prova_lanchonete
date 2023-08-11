fcb779cfb14bb14cb4528c74474b62710765540a
Vieira <kauavieira900@gmail.com>
2023-08-11 00:39:03 -0300

    Inicio da pagina principal (deu sono)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import RegisterForm from './components/RegisterForm'
+import Base from './components/Base'
+import Car from './components/Car'
 
 export default function Home() {
   return (
     <main className="flex w-full h-full flex-col items-center justify-between ">
-      <RegisterForm />
+      <div className="flex w-full  flex-col">
+        <div className="flex items-start">
+          <Base />
+        </div>
+      </div>
     </main>
   )
 }
