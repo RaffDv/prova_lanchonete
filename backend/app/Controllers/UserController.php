a38373227ea5a57f95f1e5c3732b95ee5ddbbadf
Rafael Vargas <rafael.vargas@gmail.com>
2023-08-15 23:51:04 -0300

    [update] refactored class DB estructure

--- backend/app/Controllers/UserController.php
+++ backend/app/Controllers/UserController.php
@@ -1,16 +1,27 @@
 <?php
 namespace App\Controllers;
 
-
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class UserController{
-    public function index(Request $request, Response $response){
-        $data = $request->getParsedBody();
-        $parsedData = json_encode($data);
-        $response->getBody()->write($parsedData);
+
+    
+    public function get(Request $request, Response $response){
+        $data = $GLOBALS['db']->select_sql('users', ['fields' => 'name,email'], ['id' => 7]);
+
+        
+
+        print_r($data);
+        // $parsedData = json_encode($data);
+        // $response->getBody()->write($parsedData);
+        
         return $response->withStatus(200);
+    }
+    
+    public function unique(Request $request, Response $response,array $args) {
+        
+        return $response;
     }
 
 
--- backend/public/index.php
+++ backend/public/index.php
@@ -1,11 +1,26 @@
 <?php
+header("Access-Control-Allow-Origin: {$_SERVER['REMOTE_HOST']}");
+header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE");
+header("Access-Control-Allow-Credentials: true");
 
+// imports
+
+require __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ .'/../app/models/cred.php';
+require_once __DIR__ . '/../app/models/DB.php';
+
+use App\models\BD;
 use App\Controllers\UserController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+$conx = new BD(DB_HOST, DB_USER, DB_PASS, DATABASE);
+$GLOBALS['db'] = $conx;
+
+
 
 $app = AppFactory::create();
 
@@ -14,6 +29,7 @@ $app->get('/', function (Request $request, Response $response, $args) {
     return $response;
 });
 
-$app->post('/api/hello',[UserController::class, 'index']);
+$app->get('/api/users',[UserController::class, 'get']);
+$app->get('/api/users/{id}',[UserController::class,'unique']);
 
 $app->run();
