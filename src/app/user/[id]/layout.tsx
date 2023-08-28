6a08f435c98843395ce1fdc6b3a5d039b9445ed9
Vieira <kauavieira900@gmail.com>
2023-08-28 14:40:40 -0300

    a

--- src/app/user/[id]/layout.tsx
+++ src/app/user/[id]/layout.tsx
@@ -4,9 +4,10 @@ type layoutProps = {
   children: React.ReactNode
   modal: React.ReactNode
 }
-export default function Layout({ children,modal }: layoutProps) {
+export default function Layout({ children, modal }: layoutProps) {
   return (
-    { children }
-    { modal }
-  )
+      { children }
+      {modal}
+    
+    )
 }
