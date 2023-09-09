22c2999d4a1211bd45fb38d12a4519d5fcc5ca77
Vieira <kauavieira900@gmail.com>
2023-09-09 01:09:35 -0300

    [issues 8] use edit

--- src/app/user/[id]/edit/page.tsx
+++ src/app/user/[id]/edit/page.tsx
@@ -1,22 +1,60 @@
 'use client'
-import Endereco from '@/components/Endereco'
-import Perfil from '@/components/Perfil'
-import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import logo from '@/image/logo.svg'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import api from '@/api'
 import { userType } from '@/schemas/global'
+import { Input } from '@/components/Input/Input'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-export default function Page({ params }: { params: { id: string } }) {
+const schema = z
+  .object({
+    user: z.string().min(3, 'Minímo de 3 caracteres'),
+    email: z.string().email('Por favor digite um email válido'),
+    pass: z.string().min(8, 'Minímo de 8 caracteres'),
+    confirmPass: z.string(),
+    country: z.string().min(3, 'Minímo de 3 caracteres'),
+    state: z.string().min(2, 'Somente 2 caracteres').max(2),
+    city: z.string().min(3, 'Minímo de 3 caracteres'),
+    district: z.string().min(3, 'Minímo de 3 caracteres'),
+    street: z.string().min(3, 'Minímo de 3 caracteres'),
+    num: z
+      .number({
+        errorMap: () => {
+          return {
+            message: 'Informe um número válido!',
+          }
+        },
+      })
+      .positive('Digite um número maior que 0'),
+  })
+  .refine((fields) => fields.pass === fields.confirmPass, {
+    path: ['confirmPass'],
+    message: 'As senhas precisam ser iguais!',
+  })
+
+type FormProps = z.infer<typeof schema>
+
+export default function Page({ params }: { params: { email: string } }) {
   const [page, setPage] = useState<number>(1)
   const [data, setData] = useState<userType>({} as userType)
-  const pathname = usePathname()
   const { back } = useRouter()
-  console.log(pathname)
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormProps>({
+    mode: 'all',
+    reValidateMode: 'onChange',
+    resolver: zodResolver(schema),
+  })
+
   const getdata = async () => {
-    const r = await api.user.unique({ email: params.id })
+    const r = await api.user.unique({ email: params.email })
     if (r.success) {
       setData(r.data.data)
     }
@@ -26,8 +64,13 @@ export default function Page({ params }: { params: { id: string } }) {
     getdata()
   }, [])
 
+  const handleForm = (data: FormProps) => {
+    console.log({ data })
+  }
+
   return (
     <section className="w-full h-full flex flex-col">
+      {/* Inicio do header */}
       <div className="flex m-4 items-center w-1/2 justify-between">
         <button
           onClick={back}
@@ -56,14 +99,111 @@ export default function Page({ params }: { params: { id: string } }) {
           Endereço
         </button>
       </div>
-      {page === 1 ? <Perfil /> : <Endereco />}
-      <div className="flex justify-center items-center">
-        <Link
-          href={`${pathname}/form`}
-          className="flex mt-12 w-32 h-7 bg-buttonBg border rounded-full text-font font-bold item-center justify-center"
-        >
-          Editar
-        </Link>
+      {/* Fim do header */}
+      <div className="flex flex-col justify-center items-center">
+        {page === 1 && (
+          <div className="flex flex-col justify-center items-center">
+            <form
+              className="flex flex-col items-center"
+              onSubmit={handleSubmit(handleForm)}
+            >
+              <Input
+                {...register('user')}
+                label="Usuário"
+                defaultValue={data.user}
+                helperText={errors.user?.message}
+              />
+
+              <Input
+                {...register('email')}
+                label="Email"
+                defaultValue={data.email}
+                helperText={errors.email?.message}
+              />
+
+              <Input
+                {...register('pass')}
+                label="Senha"
+                type="password"
+                defaultValue={data.pass}
+                helperText={errors.pass?.message}
+              />
+
+              <Input
+                {...register('confirmPass')}
+                label="Confirmação de senha"
+                type="password"
+                helperText={errors.confirmPass?.message}
+                required
+              />
+              <button
+                type="submit"
+                className="flex mt-12 w-32 h-7 bg-buttonBg border rounded-full text-font font-bold item-center justify-center"
+              >
+                Salvar
+              </button>
+            </form>
+          </div>
+        )}
+        {page === 2 && (
+          <div className="flex flex-col justify-center items-center">
+            <form
+              className="flex flex-col items-center"
+              onSubmit={handleSubmit(handleForm)}
+            >
+              <Input
+                {...register('country')}
+                label="País"
+                defaultValue={data.country}
+                helperText={errors.country?.message}
+              />
+
+              <Input
+                {...register('state')}
+                label="Estado"
+                defaultValue={data.state}
+                helperText={errors.state?.message}
+              />
+
+              <Input
+                {...register('city')}
+                label="Cidade"
+                defaultValue={data.city}
+                helperText={errors.city?.message}
+              />
+
+              <Input
+                {...register('district')}
+                label="Bairro"
+                defaultValue={data.district}
+                helperText={errors.district?.message}
+              />
+
+              <Input
+                {...register('street')}
+                label="Rua"
+                defaultValue={data.street}
+                helperText={errors.street?.message}
+              />
+
+              <Input
+                {...register('num', {
+                  setValueAs: (value: string) => parseInt(value, 10),
+                })}
+                label="Número"
+                type="number"
+                defaultValue={data.num}
+                helperText={errors.num?.message}
+              />
+              <button
+                type="submit"
+                className="flex mt-12 w-32 h-7 bg-buttonBg border rounded-full text-font font-bold item-center justify-center"
+              >
+                Salvar
+              </button>
+            </form>
+          </div>
+        )}
       </div>
     </section>
   )
